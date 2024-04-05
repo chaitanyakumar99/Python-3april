f4823bd050ee0ae3995363a3d61babbc659f9dc9
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-04-05 10:13:56 +0530

    Tst

--- Python Basics/Statements/Error.py
+++ Python Basics/Statements/Error.py
@@ -1,1 +1,7 @@
-print(sharuk)
+class Test{
+psvm(String[] args){
+       int a=100;
+       int b=2000;
+       int c = a+b;
+    }
+}
--- Python Basics/Statements/allidentifier.py
+++ Python Basics/Statements/allidentifier.py
@@ -1,3 +1,11 @@
-import keyword
-print(keyword.kwlist)
-print(len(keyword.kwlist))
+my_variable=100
+
+my_var9=200
+
+def my_function():
+    pass
+
+
+class my9class:
+    pass
+
